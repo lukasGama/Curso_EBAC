1b47628041a9ccd5c68d75cf5e59b237ddd57640
Lucas Gama <97904862+lukasGama@users.noreply.github.com>
2026-08-20 17:25:37 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function calculo(soma1, soma2) {
+    return soma1 + soma2;
+}
+function saudacao(nome) {
+    return `Olá, ${nome} Bem vindo ao TypeScript!`;
+}
+console.log(calculo(10, 10));
+console.log(saudacao("Lukas"));
