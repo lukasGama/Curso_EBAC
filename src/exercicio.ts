758e96d4f5fdef03bade0dbd7853cdd2f0ccc4ea
Lucas Gama <97904862+lukasGama@users.noreply.github.com>
2026-08-21 18:06:03 -0300

    Add files via upload

---
+++ src/exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome
+}
+
+console.log(multiplicar(5, 10));
+console.log(saudacao("Lukas, Bem vindo ao TypeScript!"));
