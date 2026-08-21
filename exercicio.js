74edb6084886b353f666c58218d3c65690ded5e5
Lucas Gama <97904862+lukasGama@users.noreply.github.com>
2026-08-21 18:05:12 -0300

    Delete exercicio.js

--- exercicio.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function calculo(soma1, soma2) {
-    return soma1 + soma2;
-}
-function saudacao(nome) {
-    return `Olá, ${nome} Bem vindo ao TypeScript!`;
-}
-console.log(calculo(10, 10));
-console.log(saudacao("Lukas"));
